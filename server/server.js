5fcd7b4498a0ba5c284ceb6dff3e29e8ae616c25
HP-Adam <adamhpeterman@gmail.com>
2021-08-06 23:26:17 -0500

    mutations and jason web token auth

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
 
+const { authMiddleware } = require("./utils/auth");
+
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -12,6 +14,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 // integrate our Apollo server with the Express application as middleware
